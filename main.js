89fafcdc323237b31c8769b9d2ad030c0062d8c1
salamiiwastaken <gays1328@gmail.com>
2025-01-10 13:16:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// Variables \\\
+var pageNumber = document.getElementById('textinput');
+
+//test
+console.log(pageNumber);
+
+// Functions \\
+function searchTextForString(string){
+    for(let i = 0; i < string.length-1; i++){
+        
+    }
+}
