af6ddb900e1bab4645a7b59428b6db1d46c22559
salamiiwastaken <gays1328@gmail.com>
2025-01-13 13:58:02 -0600

    j

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 // Variables \\\
-var swearWords = [crap, Crap, darn, Darn, heck, Heck, damn, Damn, hell, Hell]
-const textOutput = document.getElementById('output')
+var swearWords = ["crap", "Crap", "darn", "Darn", "heck", "Heck", "damn", "Damn", "hell", "Hell"]
+var textOutput
 
-console.log("Hi");
+// Test
+console.log("Hi. 2");
 
+//Displays the text from the file
 document.getElementById('inputfile')
-.addEventListener('change', function () {
+            .addEventListener('change', function () {
 
-    let fr = new FileReader();
-    fr.onload = function () {
-        document.getElementById('output')
-            .textContent = fr.result;
-    }
+                let fr = new FileReader();
+                fr.onload = function () {
+                    document.getElementById('output')
+                        .textContent = fr.result;
+                }
+                textOutput = document.getElementById('output').textContent;
+                replaceSwears(fr.readAsText(this.files[0]));
+                console.log(textOutput);
+            })
 
-    fr.readAsText(this.files[0]);
-})
-/*
 // Functions \\
 function replaceSwears(originalText){
-    for(let i = 0; i < swearWords.length-1; i++){
+    for(let i = 0; i < swearWords.length; i++){
         let newTextOutput
         newTextOutput = textOutput.replace(swearWords[i], "****")
-        textOutput.textContent=(newTextOutput);
+        textOutput=newTextOutput;
+        console.log("Preformed iteration "+i+".");
     }
 }
-*/
