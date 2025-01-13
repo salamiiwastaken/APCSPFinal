715e8238bbed9b62978cb5d4a1799779f2396706
salamiiwastaken <gays1328@gmail.com>
2025-01-13 12:50:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
 // Variables \\\
-var textInput = document.getElementById('textinput');
+var swearWords = [crap, darn, heck, damn, hell]
+const textOutput = document.getElementById('output')
 
-console.log("Hi");
-//test to see if this works
-textInput.addEventListener("change", function(){
-    let textContent = textInput.value;
-    console.log("The user has typed ''"+textContent+"''");
+document.getElementById('inputfile').addEventListener('change', function () {
+    let fr = new FileReader();
+    fr.onload = function () {
+        document.getElementById('output').textContent = fr.result;
+    }
+    r.readAsText(this.files[0]);
+    searchTextForString(textOutput)
 });
 
-/*
-okay i know we're going to forget this by tomorrow, so i'm gonna write it down. 
-if the getText thing works (go to console and check tomorrow), then you're going
-to use that text as a parameter to go to like "https://homestuck.com/story/<textInput>"
-to grab the text. and if that works (which, you wont know until you set up a few other things)
-then you're going to add the buttons for the 20 homestuck characters excluding the cherubs, 
-and search for both their names and troll tags, and return either a "Yes, they do appear on this page!"
-or a "No, they do not appear on this page.". then, after that's all working, just format the website 
-to look homestuck-esque, and tell Mr. Massey that we're done.
-*/
-
-
-/*
 // Functions \\
 function searchTextForString(string){
     for(let i = 0; i < string.length-1; i++){
-        
+        replace(/swearWords[i]/gi, "****")
     }
 }
-*/
+
+// there's no way this works lololol
